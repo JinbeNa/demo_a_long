3cf85036e230bb05d11a43a3054ed00e5f5c9251
unknown <nguyenngocan2509@gmail.com>
2022-11-16 06:57:37 +0700

    updatecode

--- js/form.js
+++ js/form.js
@@ -9,9 +9,42 @@ function showPass(){
 
 
 
-function myFunction(){
-  // console.log(  document.getElementById('exampleInputEmail1').value=='');
-  if(document.getElementById('exampleInputEmail1').value==''){
-    document.getElementById('error').style='block';
-  }
+function Validator(options) {
+  
+   var formElemen=document.querySelector(options.form);
+   
+   if (formElemen) {
+
+    options.rules.forEach(function (rules){
+
+      var inputElement = formElemen.querySelector(rules.selector);
+
+      if (inputElement) {
+        inputElement.onblur = function () {
+          console.log(inputElement.value);
+        }
+      }
+
+    });
+
+   }
+
+}
+
+Validator.isEmail = function (selector){
+  return {
+    selector: selector,
+    test: function (value) {
+      
+    }
+  };
+}
+
+Validator.isPassword = function (selector){
+  return {
+    selector: selector,
+    test: function (value) {
+      
+    }
+  };
 }
